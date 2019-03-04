8ba6b8e43ac7a4b7644f18fa2c8547a5b60b1632
coffman21 <fire2low@rambler.ru>
2019-03-04 16:48:01 +0300

    wip: use right path for files

--- app/src/main/java/com/banditos/piratestation/client/MainActivity.java
+++ app/src/main/java/com/banditos/piratestation/client/MainActivity.java
@@ -1,20 +1,33 @@
 package com.banditos.piratestation.client;
 
 import android.content.ComponentName;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Environment;
 import android.os.IBinder;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.banditos.piratestation.client.audio.Audio;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayerService player;
     boolean serviceBound = false;
+
+    List<Audio> audioList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,7 +61,8 @@ public class MainActivity extends AppCompatActivity {
      * Called when the user taps the Send button
      */
     public void onClickPlay(View view) {
-        playAudio("https://upload.wikimedia.org/wikipedia/commons/6/6c/Grieg_Lyric_Pieces_Kobold.ogg");
+        loadAudio();
+        // playAudio(audioList.get(0).getData());
     }
 
     private void playAudio(String media) {
@@ -63,6 +77,33 @@ public class MainActivity extends AppCompatActivity {
             //Send media with BroadcastReceiver
         }
     }
+
+    private void loadAudio() {
+        ContentResolver contentResolver = getContentResolver();
+
+        File f = new File(String.valueOf(getExternalFilesDir(Environment.DIRECTORY_MUSIC)));
+        if (!f.exists()) {
+            boolean created = f.mkdirs();
+            if (!created) Toast.makeText(this, "Folder can't be created.", Toast.LENGTH_SHORT).show();
+        }
+
+        Cursor cursor = contentResolver.query(Uri.fromFile(f), null, null, null, null);
+
+        if (cursor != null && cursor.getCount() > 0) {
+            audioList = new ArrayList<>();
+            while (cursor.moveToNext()) {
+                String data = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.DATA));
+                String title = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.TITLE));
+                String album = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ALBUM));
+                String artist = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Media.ARTIST));
+
+                // Save to audioList
+                audioList.add(new Audio(data, title, album, artist));
+            }
+        }
+        cursor.close();
+    }
+
 
     //Binding this Client to the AudioPlayer Service
     private ServiceConnection serviceConnection = new ServiceConnection() {
@@ -83,4 +124,11 @@ public class MainActivity extends AppCompatActivity {
     };
 
 
+    public void onClickPrevSong(View view) {
+        throw new UnsupportedOperationException("not implemented");
+    }
+
+    public void onClickNextSong(View view) {
+        throw new UnsupportedOperationException("not implemented");
+    }
 }
